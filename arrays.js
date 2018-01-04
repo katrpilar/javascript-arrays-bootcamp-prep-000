27bf1a0240b2837f9dacaf1d671099627c45917d
Katrina Brinson <kbrinso@gmail.com>
2018-01-04 20:22:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(-1);
-  return array;
+  const arr = array.slice(-1);
+  return arr;
 }
